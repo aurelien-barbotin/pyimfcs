a6ab72ac76031bc75a8e73d208460175ed6a7f6d
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2021-07-07 10:04:20 +0200

    Shift correction via interpolation instead of fitting

--- PyImFCS/shift_correction.py
+++ PyImFCS/shift_correction.py
@@ -7,6 +7,7 @@ Created on Thu May 27 09:33:09 2021
 """
 import tifffile
 from skimage.registration import phase_cross_correlation
+from scipy.interpolate import interp1d
 
 from skimage.registration._phase_cross_correlation import _upsampled_dft
 from scipy.ndimage import fourier_shift
@@ -17,49 +18,77 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 plt.close('all')
 
-def get_shifts(stack,ns):
+def get_shifts(stack,ns, plot=False):
     """
     Parameters:
         stack (ndarray): 3D, txy stack.
         ns (int): frame pooling used for registration, e.g 1000"""
-    ref = stack[:ns,].mean(axis=0)
+    ref = np.mean(stack[:ns,],axis=0)
     
     nt = stack.shape[0]
     nstep = nt//ns
     shifts  =list()
     for j in range(1,nstep):
-        img = stack[j*ns:(j+1)*ns].mean(axis=0)
+        # ref = np.mean(stack[(j-1)*ns:j*ns],axis=0)
+        img = np.mean(stack[j*ns:(j+1)*ns], axis=0)
         shift, error, diffphase = phase_cross_correlation(ref, img,
                                                           upsample_factor=100)
         shifts.append(shift)
+    
     shifts = np.asarray(shifts)
+    # shift = np.cumsum(shifts)
+    if plot:
+        plt.figure()
+        plt.subplot(121)
+        plt.imshow(ref)
+        plt.subplot(122)
+        plt.imshow(img)
+        plt.suptitle("shift measurement")
     return shifts
 
 def polfit(x,a,b,c):
     return a*x**2+b*x+c*x**3
 
-def registration(stack,ns):
+def registration(stack,ns, plot = False, method='interpolation'):
+    methods = ['interpolation', 'polfit']
+    if method not in methods:
+        raise KeyError('Wrong method')
+        
     shifts = get_shifts(stack, ns)
     
-    shx, shy= shifts[:,0], shifts[:,1]
+    
+    shx, shy = shifts[:,0], shifts[:,1]
     xx = np.arange(shx.size)+1
     xx*=ns
-    popt1,_ = curve_fit(polfit, xx, shx)
-    popt2,_ = curve_fit(polfit, xx, shy)
     
     xx2 = np.arange(stack.shape[0])
+    if method=='polfit':
+        popt1,_ = curve_fit(polfit, xx, shx)
+        popt2,_ = curve_fit(polfit, xx, shy)
+        
+        xh = polfit(xx2,*popt1)
+        yh = polfit(xx2,*popt2)
+        
+    elif method=="interpolation":
+        shx = np.concatenate(([0],shx))
+        shy = np.concatenate(([0],shy))
+        xx = np.arange(shx.size)*ns
+        fx = interp1d(xx, shx, fill_value = "extrapolate")
+        fy = interp1d(xx, shy, fill_value = "extrapolate")
+        
+        xh = fx(xx2)
+        yh = fy(xx2)
     
-    xh = polfit(xx2,*popt1)
-    yh = polfit(xx2,*popt2)
-    plt.figure()
-    plt.plot(xx,shifts[:,0],label="shift x",marker="o")
-    plt.plot(xx,shifts[:,1], label="shift y",marker="v")
-    plt.plot(xx2,xh,color="k",linestyle="--")
-    plt.plot(xx2,yh,color="k",linestyle="--")
-    plt.legend()
-    plt.xlabel('Frame nr')
-    plt.ylabel('shift (pixels')
-    
+    if plot:
+        plt.figure()
+        plt.plot(xx,shx,label="shift x",marker="o")
+        plt.plot(xx,shy, label="shift y",marker="v")
+        plt.plot(xx2,xh,color="k",linestyle="--")
+        plt.plot(xx2,yh,color="k",linestyle="--")
+        plt.legend()
+        plt.xlabel('Frame nr')
+        plt.ylabel('shift (pixels')
+            
     new_stack = np.zeros_like(stack)
     new_stack[0] = stack[0]
     for j in range(1,stack.shape[0]):
