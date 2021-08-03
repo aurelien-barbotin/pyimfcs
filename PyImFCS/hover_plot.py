30a8f975d396994bf0b524c586c764b6b22a74a1
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2021-08-03 13:51:23 +0200

    minor bug fix

--- PyImFCS/hover_plot.py
+++ PyImFCS/hover_plot.py
@@ -14,9 +14,9 @@ class FakeEvent():
 def multiplot_stack(stack,nsum, parn=1, normsize=2):
     
     mutable_object = {} 
-    fig,axes = plt.subplots(2,3,figsize = (10,7))
+    fig,axes = plt.subplots(2,4,figsize = (10,7))
     axes=axes.ravel()
-    
+    axes[5:] = axes[5:][::-1]
     def onclick(event):
         if event.inaxes not in axes[0:2]:
             return
@@ -29,18 +29,30 @@ def multiplot_stack(stack,nsum, parn=1, normsize=2):
         trace_raw = stack.stack[:,jj0*nsum:jj0*nsum+nsum,
                             ii0*nsum:ii0*nsum+nsum].mean(axis=(1,2))
         trace_raw = trace_raw-trace_raw.min()+trace.max()
+        print('trace raw',trace_raw.shape,trace.shape, xt.shape, xt2.shape)
         line0.set_data(ii0,jj0)
-        line1.set_data([xt,trace])
-        line10.set_data([xt2,trace_raw])
+        line1.set_data([xt2, trace_raw])
+        line10.set_data([xt, trace])
         
         axes[2].set_ylim(bottom=trace.min()*0.95, top=trace_raw.max()*1.05)
         
         axes[3].cla()
         axes[4].cla()
-        ns, corrs1, fits1 = stack.get_acf_coord(nsum,jj0,ii0)
+        axes[7].cla()
+        ns, corrs1, fits1 = stack.get_acf_coord(nsum,jj0,ii0, average=False)
         for k in range(len(ns)):
             curve = corrs1[k]
             fits = fits1[k]
+            for u in range(curve.shape[0]):
+                for v in range(curve.shape[1]):
+                    cc = curve[u,v]
+                    ff = fits[u,v]
+                    axes[7].semilogx(cc[:,0], cc[:,1]/cc[:normsize,1].mean(),
+                             label=ns[k],color="C{}".format(ns[k]))
+                    axes[7].semilogx(cc[:,0], ff/cc[:normsize,1].mean(), color="k",linestyle='--')
+            
+            curve = corrs1[k].mean(axis=(0,1))
+            fits = fits1[k].mean(axis=(0,1))
             axes[3].semilogx(curve[:,0], curve[:,1]/curve[:normsize,1].mean(),
                              label=ns[k],color="C{}".format(ns[k]))
             axes[3].semilogx(curve[:,0], fits/curve[:normsize,1].mean(), color="k",linestyle='--')
@@ -54,7 +66,10 @@ def multiplot_stack(stack,nsum, parn=1, normsize=2):
         ns, dm, ds = stack.get_param_coord(nsum,jj0,ii0,parn=1)
         axes[5].cla()
         axes[5].errorbar(ns,dm,yerr=ds,capsize=5)
-
+        
+        ns, nnm, nns = stack.get_param_coord(nsum,jj0,ii0,parn=0)
+        axes[6].cla()
+        axes[6].errorbar(ns,nnm,yerr=nns,capsize=5)
             
         axes[2].set_title("Intensity timetrace")
         axes[2].set_xlabel("Time (frames)")
@@ -70,6 +85,9 @@ def multiplot_stack(stack,nsum, parn=1, normsize=2):
         
         axes[5].set_xlabel("Binning (pixels)")
         axes[5].set_ylabel(r"$\rm D\ [\mu m^2/s]$")
+        
+        axes[6].set_xlabel("Binning (pixels)")
+        axes[6].set_ylabel(r"$\rm N$")
         
         fig.canvas.draw_idle()
         
@@ -88,9 +106,10 @@ def multiplot_stack(stack,nsum, parn=1, normsize=2):
     dt = stack.dt
     xt = np.arange(trace.size)*dt
     xt2 = np.arange(trace_raw.size)*dt
-    line1, = axes[2].plot(xt,trace)
-    line10, = axes[2].plot(xt2,trace_raw/trace_raw[0]*trace[0])
-
+    print(trace_raw.shape,trace.shape,xt.shape,xt2.shape)
+    line1, = axes[2].plot(xt,trace, label="Raw timetrace")
+    line10, = axes[2].plot(xt2,trace_raw/trace_raw[0]*trace[0], label = "corrected")
+    axes[2].legend()
     onclick(FakeEvent(axes[0]))
     fig.tight_layout()
 
