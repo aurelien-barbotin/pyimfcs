1343d703aa090a6e0bf0f3d9ee84a6dd187ccca8
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2021-10-26 10:27:06 +0200

    ?

--- PyImFCS/class_imFCS.py
+++ PyImFCS/class_imFCS.py
@@ -153,7 +153,12 @@ def get_image_metadata(path):
     description = description.split('\n')
     for d in description:
         if len(d)>1 and '=' in d:
-            k, val = d.split('=')
+            oo = d.split('=')
+            if len(oo)==2:
+                k, val = oo
+            elif len(oo)>2:
+                k = oo[0]
+                val = "=".join(oo[1:])
             k = k.strip(' ')
             val = val.strip(' ')
             try:
