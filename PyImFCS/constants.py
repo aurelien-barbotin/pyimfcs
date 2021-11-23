211ac2672ede1e32f08779469a3a7eeff464d301
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2021-11-23 17:00:38 +0100

    add path to datastets

---
+++ PyImFCS/constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 16:20:25 2021
+
+@author: aurelien
+"""
+
+datapath = "/run/user/1000/gvfs/smb-share:server=data.micalis.com,share=proced/microscopy/ZEISS/Aurelien/"
