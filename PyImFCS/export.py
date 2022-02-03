64c94dd96d302dbe408f00c330ab8b77fa03b4f5
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2022-02-03 12:52:21 +0100

    New export methods

--- PyImFCS/export.py
+++ PyImFCS/export.py
@@ -132,5 +132,59 @@ def merge_fcs_results(files, out_name, intensity_threshold = None, chi_threshold
     save_as_excel(out_name,files,nsums,all_diffs,all_chis, 
                   parameters_dict=parameters_dict)
     
-def merge_excel_results(files_list,out_name, keep_repeats=False):
-    pass
+def merge_excels(fileslist_list, out_name, keep_single_indices = False, conditions = None):
+    """Merge ther esults of FCS experiments in a single file.
+    Parameters:
+        fileslist_list (list): list of lists. Each item of the main list contains 
+            the excel files corresponding to one condition
+        out_name (str): save name
+        keep_single_indices (bool): if true, gives an indes to single acquisition. 
+            If False, single index to an excel file.
+        conditions (list): if specified, gives specific condition names 
+            (e.g control or experiment) to acquisitions"""
+    
+    if len(fileslist_list)>1 and conditions is None:
+        return ValueError('Please specify condition names')
+    assert( conditions is None or len(conditions)==len(fileslist_list))
+    
+    for inumber, files in enumerate(fileslist_list):
+        excels = [pd.ExcelFile(w) for w in files]
+        
+        # xl0 = excels[0]
+        
+        all_names = [w.sheet_names for w in excels]
+        names0 = all_names[0]
+        kept_names = list()
+        for name in names0:
+              if np.all([name in sublist for sublist in all_names]):
+                  kept_names.append(name)
+        
+        all_dfs = {}
+        for name in kept_names:
+            dfs = []
+            maxindex = 0
+            for j, xl in enumerate(excels):
+                df = xl.parse(sheet_name=name)
+                if name=="parameters":
+                    fname = files[j]
+                    df["file"] = fname
+                    
+                else:
+                    if keep_single_indices:
+                        df['repeat']+=maxindex
+                        maxindex = df['repeat'].values.max()+1
+                        # print("maxindex", maxindex)
+                    else:
+                        df['repeat'] = maxindex
+                        maxindex += 1
+                if conditions is not None:
+                    df["condition"] = conditions[inumber]
+                dfs.append(df)
+                    
+            dfs = pd.concat(dfs)
+            all_dfs[name] = dfs
+            
+    with pd.ExcelWriter(out_name) as writer:  
+            for name in kept_names:
+                df_pars = all_dfs[name]
+                df_pars.to_excel(writer, sheet_name = name)
