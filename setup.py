ef37d31d5c206c6ebe4695a20803b9bc14598b80
Aurélien Barbotin <aurelien.barbotin@inrae.fr>
2022-10-27 10:39:20 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+
+requirementPath = thelibFolder + '/lib/requirements.txt'
+install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+print(requirementPath)
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+        
 setuptools.setup(
     name="PyImFCS", # Replace with your own username
     version="0.0.1",
@@ -17,4 +27,6 @@ setuptools.setup(
     ],
     py_modules=[],
     python_requires=">=3.6",
+    packages = find_packages(),
+    install_requires=install_requires,
 )
